0260bb9568cfeef28decead6dd43abcd22ed97e1
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-10-11 10:43:19 -0400

    Improvement for production

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
     end
   end
 
+  root to: "application#fallback_index_html"
+
   get "*path", to: "application#fallback_index_html", constraints: -> (request) do
     !request.xhr? && request.format.html?
   end
